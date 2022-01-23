655c718db60f37934141284d59a62a213fc56086
root <King-Kingah>
2022-01-23 09:21:19 +0000

    Pep8

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -5,23 +5,23 @@ Python3 program to rotate a square matrix by 90 degree
 
 
 def rotate_2d_matrix(matrix):
-	''' rotate 2D matrix by 90 degrees
-	Args:
-		matrix (list[[list]]): a matrix
-	'''
-	n = len(matrix)
-	for i in range(int(n / 2)):
-		y = (n - i - 1)
-		for j in range(i, y):
-			x = (n - 1 - j)
-			# current number
-			tmp = matrix[i][j]
-			# change top for left
-			matrix[i][j] = matrix[x][i]
-			# change left for bottom
-			matrix[x][i] = matrix[y][x]
-			# change bottom for right
-			matrix[y][x] = matrix[j][y]
-			# change right for top
-			matrix[j][y] = tmp
-
+    '''
+    rotate 2d matrix by 90 degree
+    Args:
+    matrix (list[[list]]): a matrix
+    '''
+    n = len(matrix)
+    for i in range(int(n / 2)):
+        y = (n - i - 1)
+        for j in range(i, y):
+            x = (n - i - j)
+            # the number
+            temp = matrix[i][j]
+            # top for left
+            matrix[i][j] = matrix[x][i]
+            # left for bottom
+            matrix[x][i] = matrix[y][x]
+            # bottom for right
+            matrix[y][x] = matrix[j][y]
+            # right for top
+            matrix[j][y] = temp
