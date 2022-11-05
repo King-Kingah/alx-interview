e6266a2f0e7958dd403738c73bb0b6b34eaa1c51
King-Kingah <kingajames2015@gmail.com>
2022-11-05 07:13:32 +0300

    Create a Pascal's triangle with n rows & apply factorial python built in function.

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+from math import factorial
+
+# where n is the number of rows
+def pascal_triangle(n):
+    for i in range(n):
+        # first loop for leading spaces
+        for j in range(n - i + 1):
+            print(end = " ")
+
+        # second loop for row i elements
+        for j in range(i + 1):
+            # define factorial formula: nCr = n!/((n-r)!*r!)
+            print(factorial(i)//(factorial(j)*factorial(i - j)), end = " ")
+
+        # print each row in a new line
+        print("\n")
+
+
+# help(pascal_triangle)
+pascal_triangle(5)
