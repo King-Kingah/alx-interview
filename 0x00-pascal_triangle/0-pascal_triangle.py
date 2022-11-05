38d559eae5f65a6e10a6a2af2f5f7ec1d7d0a363
King-Kingah <kingajames2015@gmail.com>
2022-11-05 08:24:18 +0300

    Apply pycodestyle to further check PEP8 validation.

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -7,14 +7,11 @@ function def pascal_triangle(n)
 def pascal_triangle(n):
     """returns a list of lists of integers representing
     the Pascal’s triangle of n"""
-    # case 1: n less than 0
     if n <= 0:
         return []
-    # case 2: n equal to 1
     if n == 1:
         return [[1]]
     triangle = [[1]]
-    # case 3: n greater than 1
     for i in range(1, n):
         row = [1]
         for j in range(1, i):
